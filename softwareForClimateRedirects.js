430ed2ccbb1bd15ca11a9d93010f185c295b7ea3
Jaime (Option Zero) <jaime@optionzero.co>
2024-06-03 11:03:25 -0700

    Add final project presentation signups links

--- softwareForClimateRedirects.js
+++ softwareForClimateRedirects.js
@@ -2,7 +2,6 @@ const CURRENT_COHORT = '2024-06';
 
 const COHORT_REDIRECTS = {
     '2024-06': {
-        // Hex
         'hex-workspace': '/todo',
         'assignment-1-hex-notebook': '404',
         'assignment-2-hex-notebook': '404',
@@ -10,7 +9,6 @@ const COHORT_REDIRECTS = {
         'assignment-4a-hex-notebook': '404',
         'assignment-4b-hex-notebook': '404',
         'assignment-5-hex-notebook': '404',
-        // Airtable
         'cohort-airtable': 'https://airtable.com/appqRZmfelIxD17dP',
         'fellows-list': 'https://airtable.com/appqRZmfelIxD17dP/shroUmvQRS6orkby3',
         'fellows-form': 'https://airtable.com/appqRZmfelIxD17dP/pagnU7FXYAYMqQeqp/form',
@@ -19,23 +17,27 @@ const COHORT_REDIRECTS = {
         'final-project-plans-form': 'https://airtable.com/appqRZmfelIxD17dP/shrj0lQpITOP3Z9Al',
         'final-projects-list': 'https://airtable.com/appqRZmfelIxD17dP/shrMHmBfehRFJCbmS',
         'final-project-ratings-form': 'https://airtable.com/appqRZmfelIxD17dP/shrBdzcjZBgoq2Hf2',
+        'final-project-presentation-signup': 'https://docs.google.com/spreadsheets/d/1kIfaeswc7HOTjuyFuv7ZZqFpGMR2e7UHB0JWep1wUaI/edit#gid=739439826', // prettier-ignore
     },
     '2024-04': {
-        // Hex
         'hex-workspace': 'https://app.hex.tech/8848a05c-8000-408c-9011-f87eca4333c5',
         'assignment-1-hex-notebook': '404',
-        'assignment-2-hex-notebook': 'https://app.hex.tech/8848a05c-8000-408c-9011-f87eca4333c5/hex/dc0f8d9b-a4d1-4bb0-92fb-9a7e9487faf6/draft/logic', //prettier-ignore
+        'assignment-2-hex-notebook': 'https://app.hex.tech/8848a05c-8000-408c-9011-f87eca4333c5/hex/dc0f8d9b-a4d1-4bb0-92fb-9a7e9487faf6/draft/logic', // prettier-ignore
         'assignment-3-hex-notebook': '404',
-        'assignment-4a-hex-notebook': 'https://app.hex.tech/8848a05c-8000-408c-9011-f87eca4333c5/hex/010efa2c-5d16-4d2b-a17d-8c569ba91701/draft/logic', //prettier-ignore
-        'assignment-4b-hex-notebook': 'https://app.hex.tech/8848a05c-8000-408c-9011-f87eca4333c5/hex/5deb822e-3862-40b1-95ec-336dd0bb53cd/draft/logic', //prettier-ignore
+        'assignment-4a-hex-notebook': 'https://app.hex.tech/8848a05c-8000-408c-9011-f87eca4333c5/hex/010efa2c-5d16-4d2b-a17d-8c569ba91701/draft/logic', // prettier-ignore
+        'assignment-4b-hex-notebook': 'https://app.hex.tech/8848a05c-8000-408c-9011-f87eca4333c5/hex/5deb822e-3862-40b1-95ec-336dd0bb53cd/draft/logic', // prettier-ignore
+        'assignment-5-hex-notebook': 'https://app.hex.tech/8848a05c-8000-408c-9011-f87eca4333c5/hex/57060506-9601-4855-98cc-27cd7dd3d0f7/draft/logic', // prettier-ignore
         'cohort-airtable': 'https://airtable.com/appny4e2a2OxHugIM',
         'final-project-ideas-form': 'https://airtable.com/appny4e2a2OxHugIM/shr1medZdJbX82wDw',
         'final-project-ideas-list': 'https://airtable.com/appny4e2a2OxHugIM/shrh863LQ6vrZHqn8',
         'final-project-plans-form': 'https://airtable.com/appny4e2a2OxHugIM/shrx4Ll7Ix0NSuVg5',
         'final-projects-list': 'https://airtable.com/appny4e2a2OxHugIM/shrWZR0SlGYGRFIPi',
         'final-project-ratings-form': 'https://airtable.com/appny4e2a2OxHugIM/shrkOxQuRZqIw1LZV',
+        'final-project-presentation-signup': 'https://docs.google.com/spreadsheets/d/1SSSvtCUFxuekxBHLBMdbZU2kn4TiSxUu8itS79XU-3I/edit#gid=739439826', // prettier-ignore
     },
 };
+
+// softwarexclimate.com/links/
 
 const currentCohortRedirects = Object.entries(COHORT_REDIRECTS[CURRENT_COHORT]).map(
     ([name, url]) => {
